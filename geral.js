5d4d2bff0cb4757768e47e1ba70db3aa654731f5
teresadkla <teresafernandeslbbh@gmail.com>
2023-12-31 02:18:58 +0000

    fix em bugs

---
+++ geral.js
@@ -0,0 +1,56 @@
+/*-------------------------MENU HAMBURGUER----------------- */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const menuList = document.querySelector('.menulist');
+
+    menuToggle.addEventListener('click', function () {
+        menuList.classList.toggle('show');
+        menuToggle.classList.toggle('open');
+    });
+
+    const menuLinks = document.querySelectorAll('.menulist a');
+
+    menuLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+
+            menuList.classList.remove('show');
+            menuToggle.classList.remove('open');
+
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+});
+
+/*------------------EFEITO NAV BAR-------------------- */
+
+document.addEventListener("DOMContentLoaded", function () {
+    var h4Elements = document.querySelectorAll(".h4deskt");
+    var hrElement = document.querySelector("hr");
+    var logo = document.querySelector(".logo");
+    var headerHeight = document.querySelector("header").offsetHeight;
+
+
+    // Verifica a posição do scroll
+    window.addEventListener("scroll", function () {
+        h4Elements.forEach(function (h4Element) {
+            if (window.scrollY > headerHeight) {
+                h4Element.classList.add("cor-no-scroll");
+                hrElement.style.opacity = "0"; // Torna transparente
+                logo.style.opacity = "0";
+            } else {
+                h4Element.classList.remove("cor-no-scroll");
+                hrElement.style.opacity = "1";
+                logo.style.opacity = "1";
+            }
+        });
+    });
+});
